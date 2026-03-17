9d5235765384a35f94c56652b2ca4a89c4fbef23
dobin-sPortfolio <dobin7955gmail.com$ git config --global user.email dobin7955gmail.com>
2026-03-17 20:35:28 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Navigation from './components/Navigation'
 import Ramen from './components/Ramen'
 import Snack from './components/Snack'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       {page === "Home" && <Home  />}
       {page === "Ramen" && <Ramen  />}
       {page === "Snack" && <Snack  />}
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.content}>&copy; Dobin 2026</p>
+    </div>
+  )
+}
+
+export default Footer
